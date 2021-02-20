4c44fece756c7bd9a7862efc3dce2900896177b0
Alexander Harl <aharl.priv@gmail.com>
2021-02-20 02:48:20 +0100

    updated renderer to use helper functions

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,13 +1,13 @@
 #include "Renderer.h"
-#include <SPI.h>
+#include "Helper.h"
 
-Renderer::Renderer(int dataPin, int clkPin, int csPin, int devices)
+Renderer::Renderer(int dataPin, int clkPin, int csPin)
 {
     FLIP_H = OFF;
     FLIP_V = OFF;
     INVERT = OFF;
 
-    DEVICES = devices;
+    DEVICES = 4; // currently only matrix with 4 devices supported
 
     // initialize connected pins
     DATA_PIN = dataPin;
@@ -26,58 +26,42 @@ Renderer::Renderer(int dataPin, int clkPin, int csPin, int devices)
     clear();
 
     // initialize display
-    sendOp(DISPLAYTEST, OFF);
-    sendOp(SCANLIMIT, MAX_SCANLIMIT);
-    sendOp(DECODEMODE, OFF);
-    sendOp(SHUTDOWN, ON);
-    sendOp(INTENSITY, MAX_INTENSITY);
+    send_cmd(DISPLAYTEST, OFF);
+    send_cmd(SCANLIMIT, MAX_SCANLIMIT);
+    send_cmd(DECODEMODE, OFF);
+    send_cmd(SHUTDOWN, ON);
+    send_cmd(INTENSITY, MAX_INTENSITY);
 }
 
 void Renderer::render()
 {
     for (int row = HEIGHT; row > 0; row--)
-        renderRow(row);
+        render_row(row);
 }
 
-void Renderer::renderRow(uint8_t row)
+void Renderer::render_row(uint8_t row)
 {
     // reverse output vertically
     int index = FLIP_V ? HEIGHT - row : row - 1;
 
+    uint32_t values = buffer[index];
+
     // reverse order horizontally
-    uint32_t values = FLIP_H ? reverse(buffer[index]) : buffer[index];
+    if (FLIP_H)
+        modify_bits(REVERSE, &values);
 
-    send(dup(row), INVERT ? ~values : values);
+    // invert bits
+    values = INVERT ? ~values : values;
+
+    send(set_all_bytes(row), values);
 }
 
-void reverse(uint32_t value)
+void Renderer::send_cmd(uint8_t cmd, uint8_t value)
 {
-    uint32_t mask = 1;
-    uint32_t reversed = 0;
-
-    do
-        reversed |= (mask & value);
-    while (mask << 1);
-
-    return reversed;
+    send(set_all_bytes(cmd), set_all_bytes(value));
 }
 
-uint32_t Renderer::dup(uint8_t value)
-{
-    uint32_t values = value;
-
-    for (int i = 0; i < DEVICES - 1; i++)
-        values = values << 8 | value;
-
-    return values;
-}
-
-void Renderer::sendOp(uint8_t op, uint8_t value)
-{
-    send(dup(op), dup(value));
-}
-
-void Renderer::send(uint32_t ops, uint32_t values)
+void Renderer::send(uint32_t cmds, uint32_t values)
 {
     digitalWrite(CS_PIN, LOW);
 
@@ -85,7 +69,7 @@ void Renderer::send(uint32_t ops, uint32_t values)
     for (int i = DEVICES - 1; i >= 0; i--)
     {
         // shift out op byte at device index
-        shiftOut(DATA_PIN, CLK_PIN, MSBFIRST, ((uint8_t *)&ops)[i]);
+        shiftOut(DATA_PIN, CLK_PIN, MSBFIRST, ((uint8_t *)&cmds)[i]);
 
         // shift out value byte
         shiftOut(DATA_PIN, CLK_PIN, MSBFIRST, ((uint8_t *)&values)[i]);
