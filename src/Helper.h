26ea46a1c940da991efb34a021ea32b41528d16a
Alexander Harl <aharl.priv@gmail.com>
2021-02-20 02:46:32 +0100

    added helper

---
+++ src/Helper.h
@@ -0,0 +1,31 @@
+#ifndef Helper_h
+#define Helper_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+/**
+ * returns an int with all 4 bytes set to value
+ */
+uint32_t set_all_bytes(uint8_t value);
+
+/**
+ * Ops that can be performed on modify_bits
+ */
+enum modType_t
+{
+    CLEAR = 1,
+    WRITE = 2,
+    OVERWRITE = 3,
+    REVERSE = 4
+};
+
+/**
+ * modify bits in value
+ */
+void modify_bits(modType_t mod, uint32_t *value, uint32_t data = 0);
+
+#endif
